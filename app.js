80914d06905bc50121e293de5e922dbe9df900b6
Daniil <daniil.vodenejev@khk.ee>
2021-12-09 11:19:41 +0200

    Populated data structure items to list

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const ItemCtrl = (function (){
     }
 
     return{
+        getItems: function (){
+            return data.items
+        },
         logData: function (){
             return data
         }
@@ -24,12 +27,31 @@ const ItemCtrl = (function (){
 
 
 const UICtrl = (function (){
+    return {
+        populateItemList: function (items){
+            let html = '';
+
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="srcondary-content">
+                    <i class="edit-item fa fa-pencil"></i>
+                </a>
+                </li>`;
+            });
+
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
+
 })();
 
 const App = (function (ItemCtrl, UICtrl) {
     return {
         init: function () {
             console.log('Initializing App')
+            const items = ItemCtrl.getItems()
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl);
